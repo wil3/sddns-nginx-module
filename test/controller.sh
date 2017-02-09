ef0b157759b83b267ab77fd9593cc56748befa6c
Wil <wfkoch@gmail.com>
2017-02-09 17:10:29 -0500

    Adding additional headers

--- test/controller.sh
+++ test/controller.sh
@@ -1,1 +1,4 @@
-curl -i --header "origin: mycontroller.com" localhost
+curl -i -G -H "origin: mycontroller.com" -H "Authorization: myauth" "localhost?id=123&ip=1.2.3.4"
+#--data-urlencode "clientid=123&ipaddress=1.2.3.4" localhost
+#clientid=123&ipaddress=1.2.3.4
+#-X POST -d "clientid=123&ipaddress=1.2.3.4"
